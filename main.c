0a0957bf6b9c6d1f2c60797821ead6e3451f0181
Haizea <Haizea@192.168.0.3>
2020-03-29 18:03:22 +0200

    Funcional

--- main.c
+++ main.c
@@ -12,57 +12,102 @@
 #include <string.h>
 #include "menu.h"
 
-#define MAX_LINE 10
 
 
+
+// guarda los datos en un fichero de texto
+
+void FicheroTexto (Reserva * reserva){
+
+	FILE * fo ;
+	// a es para que se siga escribiendo y no se borre
+	fo = fopen("Reserva2", "a");
+
+	fprintf(fo, "\nReserva\n");
+	fprintf(fo, "- - - - - - - - - - -\n");
+	fprintf(fo,"Nombre: %s, ", reserva->nombre);
+	fprintf(fo, "DNI: %s, ", reserva->dni);
+	fprintf(fo, "Correo: %s, ", reserva->correo);
+	fprintf(fo, "Tarjeta: %i, ", reserva->tarjeta);
+	fprintf(fo, "Destino: %i, ", reserva->destino);
+	fprintf(fo, "Fecha Inicio: %s, ", reserva->fechaI);
+	fprintf(fo, "Numero de noches: %i, ", reserva->numeroNoches);
+	fprintf(fo, "Número de adultos: %i, ", reserva->adultos);
+	fprintf(fo, "Número de nños: %i, ", reserva->ninos);
+	fprintf(fo, "Cuna: %i, ", reserva->cuna);
+	fprintf(fo, "Tipo de pensión: %i, ", reserva->pension);
+	fprintf(fo, "Parking: %i, ", reserva->parking);
+	fprintf(fo, "Importe: %.2f €.\n",  reserva->importe);
+
+	fclose(fo);
+}
+
+
+//void FicheroBinario (Reserva * reserva){
+//
+//		FILE * fo;
+//
+//		fo = fopen("reserva", "ab" ); // abierto fichero en formato escritura
+//
+//		fwrite(reserva, sizeof(Reserva), 1, fo);
+//
+//		fclose(fo);
+//
+//		fo = fopen("reserva", "rb" );
+//
+//		//fread(reserva, sizeof(reserva), 1, fo);
+//		fread(&reserva, sizeof(reserva), 1, fo);
+//}
 
 
 int main(void) {
 
-	char * d[4];
+	// Inicializar una reserva
 
-	d[0]= (char*)malloc(sizeof(char)*7);
+	Reserva * reserva = (Reserva*)malloc(sizeof(Reserva));
 
+	// inicializar array de char de la reserva
 
+	reserva->dni = (char*)malloc(sizeof(char)*9);
+	reserva->nombre = (char*)malloc(sizeof(char)*20);
+	reserva->correo = (char*)malloc(sizeof(char)*25);
+	reserva->fechaI = (char*)malloc(sizeof(char)*15);
 
-	// d[0] = "Madrid";
-	 d[1]= "Barcelona";
-	 d[2]= "Bilbao";
-	 d[3]= "Sevilla";
-	 d[4]= "Granda";
-
-
-
-
-
-
-	//Reserva reserva;
-	Reserva * reserva[1];
 
 	seleccionarDestino(reserva);
-	//mostrarFechas(reserva);
+	seleccionarFechas(reserva);
 	seleccionarPersonas(reserva);
 	seleccionarPension(reserva);
 	seleccionarParking(reserva);
-	resumenDeDatos(reserva, d);
+	calcularImporte(reserva);
+	resumenDeDatos(reserva);
+
 	if ( continuarCompra() == 1){
 		datosComprador(reserva);
 		mostrarDatosComprador(reserva);
+		printf("¡Gracias por su compra!\n\n");
+		// solo guardamos las reservas confirmadas y completadas
+		// si le da a cancelar NO guarda en fichero
+		FicheroTexto(reserva);
+
 	}else{
 		printf("Su reserva ha sido cancelada\n ");
 	}
 
-	// guardar fichero ( escritura )
 
-	// mostrar reservas ( lectura de fichero )
-
-
-
+	// LIBERAR MEMORIA
 
 	free(reserva);
+	free(reserva->correo);
+	free(reserva->dni);
+	free(reserva->fechaI);
+	free(reserva->nombre);
+
+
+
+	printf("Fin del programa.");
+
 	return 0;
-
-
 
 }
 
